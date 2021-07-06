93bfc94c0bd8c138e9a2b1f898bdb59af086939a
GabrielQuaglio <quaglio.gabriel1304@gmail.com>
2021-07-06 12:08:42 -0300

    Alteraçao na classe campo

--- src/main/java/br/com/GabrielQuaglio/cm/modelo/Campo.java
+++ src/main/java/br/com/GabrielQuaglio/cm/modelo/Campo.java
@@ -1,4 +1,32 @@
 package br.com.GabrielQuaglio.cm.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Campo {
+
+    private final int linha;
+    private final int coluna;
+    private boolean minado;
+    private boolean aberto;
+    private boolean marcado;
+
+    private List<Campo> vizinhos = new ArrayList<>();
+
+
+    private int proximidade;
+
+    Campo(int linha, int coluna){
+        this.coluna = coluna;
+        this.linha = linha;
+
+    }
+
+    boolean adicionarVizinho(Campo vizinho){
+        return  true;
+    }
+
+
+
+
 }
--- src/main/java/br/com/GabrielQuaglio/cm/modelo/Tabuleiro.java
+++ src/main/java/br/com/GabrielQuaglio/cm/modelo/Tabuleiro.java
@@ -1,4 +1,8 @@
 package br.com.GabrielQuaglio.cm.modelo;
 
 public class Tabuleiro {
+
+    private String[][] tabuleiro = new String[16][30];
+
+
 }
